5f8c60b62c356f58b18c9920f04b8cc864ad7c33
ericyangchen <yericchenyang@gmail.com>
2023-06-05 00:50:34 +0800

    feat: add route db sql & ranking login

---
+++ internal/db/route.go
@@ -0,0 +1,215 @@
+package db
+
+import (
+	"context"
+	"time"
+
+	"github.com/CoRide-tw/backend/internal/model"
+)
+
+const createRouteTableSQL = `
+	CREATE EXTENSION IF NOT EXISTS postgis;
+
+	CREATE TABLE IF NOT EXISTS routes (
+		id SERIAL,
+		driver_id SERIAL NOT NULL,
+		start_location GEOMETRY(Point, 4326) NOT NULL,
+		end_location GEOMETRY(Point, 4326) NOT NULL,
+		start_time TIMESTAMP WITH TIME ZONE NOT NULL,
+		end_time TIMESTAMP WITH TIME ZONE NOT NULL,
+		capacity INT NOT NULL,
+		created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
+		updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
+		deleted_at TIMESTAMP WITH TIME ZONE
+	);
+
+	CREATE INDEX ON routes USING gist(start_location, end_location);
+`
+
+func initRouteTable() error {
+	if _, err := DBClient.pgPool.Exec(context.Background(), createRouteTableSQL); err != nil {
+		return err
+	}
+	return nil
+}
+
+const getRouteSQL = `
+	SELECT 
+		id, 
+		driver_id,
+		ST_X(start_location), ST_Y(start_location), 
+		ST_X(end_location), ST_Y(end_location), 
+		start_time, end_time, 
+		capacity, 
+		created_at, updated_at, deleted_at
+	FROM routes
+	WHERE id = $1 AND deleted_at IS NULL;
+`
+
+func GetRoute(id int32) (*model.Route, error) {
+	var route model.Route
+	if err := DBClient.pgPool.QueryRow(context.Background(), getRouteSQL, id).Scan(
+		&route.Id,
+		&route.DriverId,
+		&route.StartLong,
+		&route.StartLat,
+		&route.EndLong,
+		&route.EndLat,
+		&route.StartTime,
+		&route.EndTime,
+		&route.Capacity,
+		&route.CreatedAt,
+		&route.UpdatedAt,
+		&route.DeletedAt,
+	); err != nil {
+		return nil, err
+	}
+	return &route, nil
+}
+
+// Note: ST_MakePoint(longitude, latitude)
+const listNearestRouteSQL = `
+	WITH rider_requirements AS (
+		SELECT 
+			ST_SetSRID(ST_MakePoint($1, $2), 4326) AS pickup_point,
+			ST_SetSRID(ST_MakePoint($3, $4), 4326) AS dropoff_point,
+			$5::timestamp with time zone AS pickup_start_time,
+			$6::timestamp with time zone AS pickup_end_time
+	)
+	SELECT 
+		id,
+		driver_id,
+		ST_X(start_location),
+		ST_Y(start_location),
+		ST_X(end_location),
+		ST_Y(end_location),
+		start_time,
+		end_time,
+		capacity,
+		created_at,
+		updated_at,
+		deleted_at
+	FROM 
+		routes, rider_requirements
+	WHERE 
+		deleted_at IS NULL 
+		AND start_time <= (SELECT pickup_start_time FROM rider_requirements)
+		AND end_time >= (SELECT pickup_end_time FROM rider_requirements)
+	ORDER BY (
+		ST_Distance(start_location, (SELECT pickup_point FROM rider_requirements)) + 
+		ST_Distance(end_location, (SELECT dropoff_point FROM rider_requirements))
+	) ASC
+	LIMIT 30;
+`
+
+func ListNearestRoutes(
+	pickupLong, pickupLat, dropoffLong, dropoffLat float64,
+	pickupStartTime, pickupEndTime time.Time,
+) ([]*model.Route, error) {
+	rows, err := DBClient.pgPool.Query(context.Background(), listNearestRouteSQL,
+		pickupLong, pickupLat, dropoffLong, dropoffLat, pickupStartTime, pickupEndTime)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var routes []*model.Route
+	for rows.Next() {
+		var route model.Route
+		if err := rows.Scan(
+			&route.Id,
+			&route.DriverId,
+			&route.StartLong,
+			&route.StartLat,
+			&route.EndLong,
+			&route.EndLat,
+			&route.StartTime,
+			&route.EndTime,
+			&route.Capacity,
+			&route.CreatedAt,
+			&route.UpdatedAt,
+			&route.DeletedAt,
+		); err != nil {
+			return nil, err
+		}
+		routes = append(routes, &route)
+	}
+	return routes, nil
+}
+
+const createRouteSQL = `
+	INSERT INTO routes (driver_id, start_location, end_location, start_time, end_time, capacity)
+	VALUES (
+		$1, 
+		ST_SetSRID(ST_MakePoint($2, $3), 4326), 
+		ST_SetSRID(ST_MakePoint($4, $5), 4326), 
+		$6, 
+		$7, 
+		$8
+	)
+	RETURNING id, created_at, updated_at;
+`
+
+func CreateRoute(route *model.Route) (*model.Route, error) {
+	if err := DBClient.pgPool.QueryRow(context.Background(), createRouteSQL,
+		route.DriverId,
+		route.StartLong,
+		route.StartLat,
+		route.EndLong,
+		route.EndLat,
+		route.StartTime,
+		route.EndTime,
+		route.Capacity,
+	).Scan(
+		&route.Id,
+		&route.CreatedAt,
+		&route.UpdatedAt,
+	); err != nil {
+		return nil, err
+	}
+	return route, nil
+}
+
+const updateRouteSQL = `
+	UPDATE routes SET
+		start_lat = $1,
+		start_long = $2,
+		end_lat = $3,
+		end_long = $4,
+		start_time = $5,
+		end_time = $6,
+		capacity = $7,
+		updated_at = NOW()
+	WHERE id = $8, AND driver_id = $9
+	RETURNING *;
+`
+
+func UpdateRoute(route *model.Route) (*model.Route, error) {
+	if err := DBClient.pgPool.QueryRow(context.Background(), updateRouteSQL,
+		route.StartLat,
+		route.StartLong,
+		route.EndLat,
+		route.EndLong,
+		route.StartTime,
+		route.EndTime,
+		route.Capacity,
+		route.Id,
+		route.DriverId,
+	).Scan(
+		&route.Id,
+		&route.DriverId,
+		&route.StartLat,
+		&route.StartLong,
+		&route.EndLat,
+		&route.EndLong,
+		&route.StartTime,
+		&route.EndTime,
+		&route.Capacity,
+		&route.CreatedAt,
+		&route.UpdatedAt,
+		&route.DeletedAt,
+	); err != nil {
+		return nil, err
+	}
+	return route, nil
+}
