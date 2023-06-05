8188800873a57f0d4528070b40e36dfaefdc72f0
ericyangchen <yericchenyang@gmail.com>
2023-06-06 06:06:46 +0800

    fix: remove trailing slash

--- internal/router/request.go
+++ internal/router/request.go
@@ -3,9 +3,9 @@ package router
 func (r *router) setRequestRoutes() {
 	requestRouter := r.Engine.Group("/request")
 
-	requestRouter.GET("/", r.Service.Request.List)
+	requestRouter.GET("", r.Service.Request.List)
 	requestRouter.GET("/:id", r.Service.Request.Get)
-	requestRouter.POST("/", r.Service.Request.Create)
+	requestRouter.POST("", r.Service.Request.Create)
 	requestRouter.PATCH("/:id", r.Service.Request.Update)
 	requestRouter.PATCH("/:id/status", r.Service.Request.Deny)
 	requestRouter.DELETE("/:id", r.Service.Request.Delete)
--- internal/router/route.go
+++ internal/router/route.go
@@ -5,7 +5,7 @@ func (r *router) setRouteRoutes() {
 
 	routeRouter.GET("/ranking", r.Service.Route.ListNearestRoutes)
 	routeRouter.GET("/:id", r.Service.Route.Get)
-	routeRouter.POST("/", r.Service.Route.Create)
+	routeRouter.POST("", r.Service.Route.Create)
 	routeRouter.PATCH("/:id", r.Service.Route.Update)
 	routeRouter.DELETE("/:id", r.Service.Route.Delete)
 }
