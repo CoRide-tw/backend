29839fe7dd07246e55522081ac956c2ca089867e
ericyangchen <yericchenyang@gmail.com>
2023-06-05 16:55:06 +0800

    feat: add request service

--- internal/service/main.go
+++ internal/service/main.go
@@ -1,13 +1,15 @@
 package service
 
 type Service struct {
-	User  *userSvc
-	Route *routeSvc
+	User    *userSvc
+	Route   *routeSvc
+	Request *requestSvc
 }
 
 func NewService() *Service {
 	return &Service{
-		User:  &userSvc{},
-		Route: &routeSvc{},
+		User:    &userSvc{},
+		Route:   &routeSvc{},
+		Request: &requestSvc{},
 	}
 }
