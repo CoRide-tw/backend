932bf640a5e88a702fa6629289af7fccaab89c33
ericyangchen <yericchenyang@gmail.com>
2023-06-05 16:53:57 +0800

    chore: add request constants

---
+++ internal/constants/request.go
@@ -0,0 +1,12 @@
+package constants
+
+const (
+	// RequestStatusPending is the status of a request that has not been accepted by a driver
+	RequestStatusPending = "pending"
+	// RequestStatusAccepted is the status of a request that has been accepted by a driver
+	RequestStatusAccepted = "accepted"
+	// RequestStatusCompleted is the status of a request that has been completed by a driver
+	RequestStatusCompleted = "completed"
+	// RequestStatusCancelled is the status of a request that has been cancelled by a rider
+	RequestStatusCancelled = "cancelled"
+)
