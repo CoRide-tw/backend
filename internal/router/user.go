f6403d59b636ed82e4da9486b332f1167a97c9bb
ericyangchen <yericchenyang@gmail.com>
2023-06-06 01:40:30 +0800

    fix: http method

--- internal/router/user.go
+++ internal/router/user.go
@@ -4,5 +4,5 @@ func (r *router) setUserRoutes() {
 	userRouter := r.Engine.Group("/user")
 
 	userRouter.GET("/:id", r.Service.User.Get)
-	userRouter.PUT("/:id", r.Service.User.Update)
+	userRouter.PATCH("/:id", r.Service.User.Update)
 }
