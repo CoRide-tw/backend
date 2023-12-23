6dadb2a3bfd4b0fda13c1ca0276829a30fd65b8a
DenChenn <aaaa102234@gmail.com>
2023-12-23 16:55:14 +0800

    feat: add health check

--- internal/router/login.go
+++ internal/router/login.go
@@ -1,6 +1,13 @@
 package router
 
+import "github.com/gin-gonic/gin"
+
 func (r *router) setLoginRoutes() {
+	r.Engine.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Welcome to CoRide API service, health check passed.",
+		})
+	})
 	r.Engine.GET("/oauthUrl", r.Service.User.OauthUrl)
 	r.Engine.POST("/user/login", r.Service.User.OAuthLogin)
 }
